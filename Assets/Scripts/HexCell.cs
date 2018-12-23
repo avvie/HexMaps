3230eff6b6a862acfb5424f4e4c003cfcc9299b6
avvie <akonofagos@gmail.com>
2018-12-23 10:31:15 +0200

    first commit

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour{
+    public HexCoordinates coordinates;
+    public Color color;
+    [SerializeField]
+    HexCell[] neighbours;
+
+    public HexCell GetNeighbor(HexDirection direction) {
+        return neighbours[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell) {
+        neighbours[(int)direction] = cell;
+        cell.neighbours[(int)direction.Opposite()] = this;
+    }
+
+}
---
+++ Assets/Scripts/HexStatics.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexStatics{
+
+    public const float outerRadius = 10f;
+    public static float innerRadius = outerRadius * (Mathf.Sqrt(3) / 2);
+
+    public const float solidFactor = 0.75f;
+    public const float blendFactor = 1f - solidFactor;
+
+    //orientation with point up
+    static Vector3[] corners = {
+        new Vector3(0, 0, outerRadius),
+        new Vector3(innerRadius, 0, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0, -0.5f * outerRadius),
+        new Vector3(0, 0, -outerRadius),
+        new Vector3(-innerRadius, 0, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0, 0.5f * outerRadius),
+        new Vector3(0, 0, outerRadius)
+    };
+
+    public static Vector3 GetFirstCorner(HexDirection direction) {
+        return corners[(int)direction];
+    }
+
+    public static Vector3 GetSecondCorner(HexDirection direction) {
+        return corners[(int)direction + 1];
+    }
+
+    public static Vector3 GetFirstSolidCorner(HexDirection direction) {
+        return corners[(int)direction] * solidFactor;
+    }
+
+    public static Vector3 GetSecondSolidCorner(HexDirection direction) {
+        return corners[(int)direction + 1] * solidFactor;
+    }
+}
